b70ebf214c8ffa770211754635d7c94952aaf888
rana-arju <arjurana20@gmail.com>
2025-06-02 21:55:14 +0600

    vercel json file add for deploy on vercel

--- src/app/modules/User/user.controller.ts
+++ src/app/modules/User/user.controller.ts
@@ -1,23 +1,17 @@
 import { Request, Response } from "express";
 import { adminServices } from "./user.service";
+import catchAsync from "../../../shared/catchAsync";
+import sendResponse from "../../../shared/sendResponse";
 
-const createAdmin = async (req: Request, res: Response) => {
-  try {
-    const result = await adminServices.createAdmin(req.body);
-    res.status(200).json({
-      success: true,
-      message: "User created successfully",
-      data: result,
-    });
-  } catch (error) {
-    console.error("Error creating admin:", error);
-    return res.status(500).json({
-      success: false,
-      message: "Internal server error",
-      error: error instanceof Error ? error.message : "Unknown error",
-    });
-  }
-};
+const createAdmin = catchAsync(async (req: Request, res: Response) => {
+  const result = await adminServices.createAdmin(req.body);
+  sendResponse(res, {
+    statusCode: 201,
+    success: true,
+    message: "User created successfully",
+    data: result,
+  });
+});
 
 export const adminController = {
   createAdmin,
