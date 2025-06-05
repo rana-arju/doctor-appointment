f80bd86a5159e9b6712a5a0a44696d9bd4b1c876
rana-arju <arjurana20@gmail.com>
2025-06-05 10:18:53 +0600

    auth gurd add on admin route

--- src/app/modules/Admin/admin.route.ts
+++ src/app/modules/Admin/admin.route.ts
@@ -3,18 +3,36 @@ import { adminController } from "./admin.controller";
 import { z, AnyZodObject } from "zod";
 import { validateRequest } from "../../../middlewares/validationRequest";
 import { adminUpdate } from "./admin.validation";
+import auth from "../../../middlewares/auth";
+import { UserRole } from "@prisma/client";
 
 const router = express.Router();
 
-
-router.get("/", adminController.getAllAdminFromDB);
-router.get("/:id", adminController.getSingleAdminFromDB);
+router.get(
+  "/",
+  auth(UserRole.ADMIN, UserRole.SUPER_ADMIN),
+  adminController.getAllAdminFromDB
+);
+router.get(
+  "/:id",
+  auth(UserRole.ADMIN, UserRole.SUPER_ADMIN),
+  adminController.getSingleAdminFromDB
+);
 router.patch(
   "/:id",
+  auth(UserRole.ADMIN, UserRole.SUPER_ADMIN),
   validateRequest(adminUpdate),
   adminController.updateSingleAdminFromDB
 );
-router.delete("/:id", adminController.deleteSingleAdminFromDB);
-router.delete("/soft/:id", adminController.softDeleteSingleAdminFromDB);
+router.delete(
+  "/:id",
+  auth(UserRole.ADMIN, UserRole.SUPER_ADMIN),
+  adminController.deleteSingleAdminFromDB
+);
+router.delete(
+  "/soft/:id",
+  auth(UserRole.ADMIN, UserRole.SUPER_ADMIN),
+  adminController.softDeleteSingleAdminFromDB
+);
 
 export const adminRoutes = router;
