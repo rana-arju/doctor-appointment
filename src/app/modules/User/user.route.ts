c26c6a638a03b3c647446865b85d58bd2ae288d7
rana-arju <arjurana20@gmail.com>
2025-06-05 21:06:43 +0600

    (Rana Arju) - doctor and patient validation complete using zod

--- src/app/modules/User/user.route.ts
+++ src/app/modules/User/user.route.ts
@@ -7,15 +7,22 @@ import { userValidation } from "./user.validation";
 const router = express.Router();
 
 router.post(
-  "/",
-  auth(UserRole.ADMIN, UserRole.SUPER_ADMIN), 
+  "/create-admin",
+  auth(UserRole.ADMIN, UserRole.SUPER_ADMIN),
   fileUploader.upload.single("file"),
   (req: Request, res: Response, next: NextFunction) => {
-    req.body = userValidation.createAdmin.parse(JSON.parse(req.body.data))
-    return adminController.createAdmin(req, res, next)
-
+    req.body = userValidation.createAdmin.parse(JSON.parse(req.body.data));
+    return adminController.createAdmin(req, res, next);
   }
- 
+);
+router.post(
+  "/create-doctor",
+  auth(UserRole.ADMIN, UserRole.SUPER_ADMIN),
+  fileUploader.upload.single("file"),
+  (req: Request, res: Response, next: NextFunction) => {
+    req.body = userValidation.createDoctor.parse(JSON.parse(req.body.data));
+    return adminController.createAdmin(req, res, next);
+  }
 );
 
 export const userRoutes = router;
